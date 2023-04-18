4f8728ed200b12aa854a34919c94735cc5f9fe1f
Facuzogbe <facundozogbe@gmail.com>
2023-04-18 20:42:19 -0300

    click with redirection to youtube videos

--- src/assets/main.js
+++ src/assets/main.js
@@ -23,27 +23,28 @@ async function fetchData(API) {
         let view = `
         ${videos.items.map(video => `
             <div class="group relative">
-            <div
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-             <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                <a href="https://www.youtube.com/watch?v=${video.id.videoId}" target="_blank">
+                  <div class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+                  </div>
+                    <div class="mt-4 flex justify-between">
+                        <h3 class="text-2xl text-gray-900">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                ${video.snippet.channelTitle}
+                        </h3>
+                    </div>
+                      <div class="mt-4 flex justify-between">
+                        <h3 class="text-sm text-gray-700">
+                                <span aria-hidden="true" class="absolute inset-0"></span>
+                                ${video.snippet.title}
+                        </h3>
+                    </div>
+                </a>
             </div>
-            <div class="mt-4 flex justify-between">
-            <h3 class="text-2xl text-gray-900">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                ${video.snippet.channelTitle}
-            </h3>
-            </div>
-            <div class="mt-4 flex justify-between">
-            <h3 class="text-sm text-gray-700">
-                <span aria-hidden="true" class="absolute inset-0"></span>
-                ${video.snippet.title}
-            </h3>
-            </div>
-        </div>
         `).slice(0,4).join('')}
-      `;
-      content.innerHTML = view;
+        `;
+        content.innerHTML = view;
     } catch (error) {
         console.log(error);
     }
-}) ();
+})();
